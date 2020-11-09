3c3e25059a8cb648e26420c5573a9dbe194cd0bf
rodrigo <rodrigo.davila@globant.com>
2020-11-09 19:55:27 -0300

    se agrego nav-bar y home page

--- WebApplicationCLIP/AppAngularCLIP/src/app/app.module.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginBox} from './components/login-box/login.component';
+import { LoginBox } from './components/login-box/login.component';
 import { LoginService } from './services/login.service';
 
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginBox
+    LoginBox,
+    NavBarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomePageComponent },
+      { path: 'login', component: LoginBox }
+    ])
   ],
   providers: [[LoginService]],
   bootstrap: [AppComponent]
