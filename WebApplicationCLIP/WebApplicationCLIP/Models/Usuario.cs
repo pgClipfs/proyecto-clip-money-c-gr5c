0799e395c5b1c0caf5eee5e42a2d11ac28529dab
Maxi Miraglia <maxi_miraglia@hotmail.com>
2020-11-14 18:40:38 -0300

    se agrega clase Usuario

---
+++ WebApplicationCLIP/WebApplicationCLIP/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationCLIP.Models
+{
+    public class Usuario
+    {
+        public Usuario(string id, string nombre, string apellido, string dni, string nombreDeUsuario, string contraseña)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Dni = dni;
+            SitCrediticia = null;
+            NombreDeUsuario = nombreDeUsuario;
+            Contraseña = contraseña;
+        }
+
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public string SitCrediticia { get; set; }
+        public string NombreDeUsuario { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Contraseña { get; set; }
+
+    }
+}
