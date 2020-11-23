7ac93118b9fcb8f871e809f7ab533f9a50bea46b
rodrigo <rodrigo.davila@globant.com>
2020-11-23 11:56:08 -0300

    Se terminaron de agregar las validaciones al formulario de registro

--- WebApplicationCLIP/AppAngularCLIP/src/app/components/signup-page/signup-page.component.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/components/signup-page/signup-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,Validators, FormGroup, FormsModule} from '@angular/forms'
+import { FormBuilder, Validators, FormGroup, FormsModule } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router';
 import { stringify } from 'querystring';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-signup-page',
@@ -9,40 +11,51 @@ import { stringify } from 'querystring';
 })
 export class SignupPageComponent implements OnInit {
 
+  clavesNoCoinciden: boolean = false;
   fgroup: FormGroup; //Agrupa los datos de carga del formulario
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,private route: ActivatedRoute, private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
     //Se le indica al formGroup que vamos a crear con el FormBuilder un grupo de elementos
     // de carga constituido por un objeto json que va a contener los campos.
     this.fgroup = this.fb.group({
-      NombreUsuario: '',
-      Nombre: ['',[Validators.required,Validators.maxLength(55),Validators.pattern("^[a-zA-Z' ]+$")]],
-      Apellido: '',
-      Email: '',
-      Dni: null,
-      Telefono: null,
-      Contrasena: '',
-      ContrasenaRep: '',
-      image: ''
-      
+      NombreUsuario: ['aaaaaaaa', [Validators.required, Validators.maxLength(30), Validators.minLength(8), Validators.pattern("^[a-zA-Z0-9_-]+$")]],
+      Nombre: ['asd', [Validators.required, Validators.maxLength(55), Validators.pattern("^[a-zA-Z' ]+$")]],
+      Apellido: ['asd', [Validators.required, Validators.maxLength(55), Validators.pattern("^[a-zA-Z' ]+$")]],
+      Email: ['ads@asd', [Validators.required, Validators.maxLength(55), Validators.email]],
+      Dni: ['12345678', [Validators.required, Validators.minLength(8), Validators.pattern("^[0-9]{1,8}$")]],
+      Telefono: ['126552', [Validators.pattern("^[0-9]{1,20}$")]],
+      Contrasena: ['', [Validators.required, Validators.maxLength(55)]],
+      ContrasenaRep: ['', [Validators.required, Validators.maxLength(55)]],
+      image: ['',]
 
     })
   }
+ 
+  grabar() {
+    if (this.fgroup.valid) {
 
-  grabar(){
-    if(this.fgroup.valid ){
-      console.log(this.fgroup.value)
+      if (this.fgroup.value.Contrasena != this.fgroup.value.ContrasenaRep) {
+        this.clavesNoCoinciden = true;
+        console.log("Las contraseñas no coinciden")
+      } else {
+        this.clavesNoCoinciden = false;
+      }      
+      
+      console.log("Registrando Usuario");
+
+      var returnUrl = this.route.snapshot.queryParams.returnUrl || '/principal';
+        if(this.loginService.usuarioLogueado){
+            this.router.navigate([returnUrl]);
+
+
     }
-    else{
+    else {
       console.log("Hay datos invalidos")
     }
-    
   }
 
-  
-
-  LimpiarForm(){
+  LimpiarForm() {
     this.fgroup.reset();
   }
 }
