583d56c14bf46ad9e442489ae287d69bef1da361
juancuello98 <jcuello673@gmail.com>
2020-12-15 01:57:58 -0300

    Realizo la base para la pagina principal con su estilo

--- WebApplicationCLIP/AppAngularCLIP/src/app/components/navbar-lateral/navbar-lateral.component.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/components/navbar-lateral/navbar-lateral.component.ts
@@ -12,6 +12,13 @@ export class NavbarLateralComponent implements OnInit {
   nombreUsuario = 'juancuello98';
   saldoPesos = 2555;
   opened = false;
+  numeroCuenta = 201241535212312;
+  numeroCuentaDol = 20124134534512;
+  nombreApellido = 'JUAN CRUZ CUELLO';
+  Fecha = '14/12/2020';
+  Descripcion = 'Ingreso de dinero';
+  Monto= '$2500';
+  tipoOperacion = 'Ingr.';
   
   abrirSideBar(){
     this.opened = !this.opened;
