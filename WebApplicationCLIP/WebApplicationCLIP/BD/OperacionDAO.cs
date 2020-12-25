4b4348a447891994adc0726ba0d1598b49cfde90
rodrigo <rodrigo.davila@globant.com>
2020-12-25 18:42:11 -0300

    correcciones en operacionDAO

--- WebApplicationCLIP/WebApplicationCLIP/BD/OperacionDAO.cs
+++ WebApplicationCLIP/WebApplicationCLIP/BD/OperacionDAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,113 @@ using WebApplicationCLIP.Models;
 
 namespace WebApplicationCLIP.BD
 {
-    public interface OperacionDAO : CRUD<Cuenta>
+    public class OperacionDAO : CRUD<Operacion>
     {
 
+        public Operacion consultar(Cuenta t)
+        {
+            string script = "SELECT * FROM CUENTAS WHERE CVU = " + "'" + t.Cvu + "'";
+
+            Operacion operacionResultado=null;
+
+            ConexionBD conexion = new ConexionBD();
+            conexion.abrir();
+
+            try
+            {
+                SqlCommand comando = new SqlCommand(script, conexion.conexionBD);
+                SqlDataReader lector = comando.ExecuteReader();
+                List<string> ensamblador = new List<string>();
+                while (lector.Read())
+                {
+                    for (int i = 0; i < lector.FieldCount; i++)
+                    {
+                        if (i == 1)
+                        {
+                            string dni = lector.GetValue(1).ToString();
+                            i++;
+                        }
+                        ensamblador.Add(lector.GetValue(i).ToString());
+                    }
+
+                }
+                if (ensamblador.Count > 0)
+                {
+                    conexion.cerrar();
+                    t = Cuenta.ensamblarCuenta(ensamblador, t);
+
+                    //aca hay que "ensamblar" el objeto operacion resultado
+                    //operacionResultado = algo
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al ejecutar la consulta --> " + e.Message);
+            }
+
+            conexion.cerrar();
+            return operacionResultado;
+        }
+
+        public Operacion consultar(Operacion t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void eliminar(Operacion t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Operacion> listarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void modificar(Operacion t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string obtenerUltimoCVU()
+        {
+            string script = "SELECT TOP 1 * FROM CUENTAS ORDER BY CVU DESC";
+            string ultimoCVU;
+
+            ConexionBD conexion = new ConexionBD();
+            conexion.abrir();
 
 
+            try
+            {
+                SqlCommand comando = new SqlCommand(script, conexion.conexionBD);
+                SqlDataReader lector = comando.ExecuteReader();
+                List<string> ensamblador = new List<string>();
+                while (lector.Read())
+                {
+                    for (int i = 0; i < lector.FieldCount; i++)
+                    {
+                        ensamblador.Add(lector.GetValue(i).ToString());
+                    }
+                }
+                if (ensamblador.Count > 0)
+                {
+                    conexion.cerrar();
+                    ultimoCVU = ensamblador[0];
+                    return ultimoCVU;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al ejecutar la consulta --> " + e.Message);
+            }
+            conexion.cerrar();
+            return "ERROR";
+        }
+
+        public void registrar(Operacion t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
