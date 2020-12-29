5f0226fa53b4d4539a49af2588c129c89cf3f661
rodrigo <rodrigo.davila@globant.com>
2020-12-29 17:24:55 -0300

    mejora del servicio de operaciones
    ahora carga correctamente el tipo de operacion a cada operacion, antes de devolver el array

--- WebApplicationCLIP/AppAngularCLIP/src/app/services/operacion.service.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/services/operacion.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { HttpClientModule } from '@angular/common/http';
 import { Usuario } from '../modelos/usuario';
+import { Operacion } from '../clases';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map, retry, catchError } from 'rxjs/operators';
 import { LoginService } from './login.service';
@@ -16,7 +17,7 @@ export class OperacionService {
 
   constructor(private http: HttpClient, private loginService: LoginService) { }
 
-  public getOperacionesCvu(cvu: string): Observable<any> {
+  public getOperacionesCvu(cvu: string): Observable<Operacion[]> {
 
     //no es lo ideal pasar los parametros asi, pero no logre que funcione de otra forma
     return this.http.post<any>(this.urlApi + 'get/operaciones?cvu='+cvu, { })
@@ -26,7 +27,27 @@ export class OperacionService {
           return err;
         }),
         map(ops => {
-          //operaciones: Operacion[]          
+          ops.forEach(op => {
+            switch(op.TipoOperacion){
+              case 1:{
+                op.Tipo="Transferencia";
+                break;
+              }
+              case 2:{
+                op.Tipo="Depósito";
+                break;
+              }
+              case 3:{
+                op.Tipo="Extracción";
+                break;
+              }
+              case 4:{
+                op.Tipo="Giro al Descubierto";
+                break;
+              }
+            }
+          });
+
           return ops;
         }
         )
