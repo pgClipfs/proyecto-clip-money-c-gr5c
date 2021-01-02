115b120c3483da115926f4bf6571a9255c532fb6
juancuello98 <jcuello673@gmail.com>
2021-01-02 20:52:28 -0300

    Realizo diseno del formulario, agrego campos, arreglo de estructura , arreglo de la pagina principal

--- WebApplicationCLIP/AppAngularCLIP/src/app/components/pagina-principal/pagina-principal.component.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/components/pagina-principal/pagina-principal.component.ts
@@ -22,7 +22,7 @@ export class PaginaPrincipalComponent implements OnInit {
   opened = false;
   numeroCuenta = 201241535212312;
   numeroCuentaDol = 20124134534512;
-  nombreApellido = 'JUAN CRUZ CUELLO';
+  nombreApellido = 'Juan Cruz Cuello';
   Fecha = '14/12/2020';
   Descripcion = 'Ingreso de dinero';
   Monto= '$2500';
