a6152747f3f0813524342cb3ab56d3afa233c70a
juancuello98 <jcuello673@gmail.com>
2021-01-02 03:14:19 -0300

    Formulario para ingreso/retiro dinero, primeras validaciones y conexion con servicio para datos de cuenta, falta diseno

--- WebApplicationCLIP/AppAngularCLIP/src/app/components/pantalla-ingreso-egreso-dinero/pantalla-ingreso-egreso-dinero.component.ts
+++ WebApplicationCLIP/AppAngularCLIP/src/app/components/pantalla-ingreso-egreso-dinero/pantalla-ingreso-egreso-dinero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, FormsModule } from '@angular/forms'
 import { Usuario } from '../../modelos/usuario';
+import {CuentaService} from '../../services/cuenta.service'
 
 @Component({
   selector: 'app-pantalla-ingreso-egreso-dinero',
@@ -9,43 +10,74 @@ import { Usuario } from '../../modelos/usuario';
 })
 export class PantallaIngresoEgresoDineroComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private cuentaService: CuentaService) { }
 
   cuentaOrigen : Usuario;
-  origenDepositos : Array<string> = ['$ - Cuello Juan','U$D - Ramirez Juan'];
+  tiposOperaciones : Array<string> = ['Deposito','Extraccion'];
   isSubmitted = false;
+  labelBotonCuenta = 'Ver datos de cuenta';
+  verDatosCuenta = false;
+ // nombreCuenta = '';
+  //apellidoCuenta ='';
+  cvuCuenta = '';
+  saldoCuenta : number = 0;
+  usuarioCuenta = '';
 
   formRetiroDinero : FormGroup;
   formDinero : FormGroup;
   ngOnInit(): void {
     this.formDinero = this.fb.group({
-      cvuDestino: ['', [Validators.required]],
-      CuentaOrigen: ['', [Validators.required]],
-      origenDeposito: ['', [Validators.required]],
+      inputCVU: ['', [Validators.required],[Validators.maxLength(22)],[Validators.minLength(22)]],
+      selectorTipoOperacion: ['', [Validators.required]],
       Monto: ['', [Validators.required, Validators.pattern("^[0-9]{1,8}$")]]
     })
   }
 
-  get origenDeposito() {
-    return this.formDinero.get('origenDeposito');
+  get selectorTipoOperacion() {
+    return this.formDinero.get('selectorTipoOperacion');
   }
+
+  get inputCVU() {
+    return this.formDinero.get('inputCVU');
+  }
+
+
 
   public changeCuenta(e) {
 
-    this.origenDeposito.setValue(e.target.value, {
+    this.selectorTipoOperacion.setValue(e.target.value, {
       onlySelf: true
     })
   }
 
   onSubmit() {
 
-      console.log(JSON.stringify(this.origenDeposito.value))
+      console.log(JSON.stringify(this.selectorTipoOperacion.value))
 
 
   }
 
   public LimpiarForm(){
     this.formDinero.reset();
+  }
+
+  public verCuenta(){
+    this.cuentaService.obtenerCuenta(this.inputCVU.value).subscribe(
+      cuenta => {
+        this.cvuCuenta = cuenta.Cvu;
+        this.saldoCuenta = cuenta.Saldo;
+        this.usuarioCuenta = cuenta.NombreUsuario;
+      }
+
+    )
+    this.verDatosCuenta = !this.verDatosCuenta;
+    if( this.verDatosCuenta == true){
+      this.labelBotonCuenta = 'Esconder datos de cuenta';
+    }
+    else{
+      this.labelBotonCuenta = 'Ver datos de cuenta';
+    }
+
   }
 
 }
